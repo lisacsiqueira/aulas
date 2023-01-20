109716671e91131ab7caefeaf8628ca77d6bd93d
Lisa Costa <112580152+lisacsiqueira@users.noreply.github.com>
2023-01-20 13:11:01 -0300

    test

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -19,4 +19,10 @@ public class Calculadora {
      * @param num02
      * @return O resultado deste método é a soma dos dois números
      */
+
+    public int somar(int num01, int num02) {
+        return num01 + num02;
+    }
+
+    
 }
