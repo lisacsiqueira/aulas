da42fb13e30fb2c8a44559716c268ec2d0ded303
Lisa Costa <112580152+lisacsiqueira@users.noreply.github.com>
2023-01-20 11:45:05 -0300

    mensagens

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas
+ * entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentaçãodesta classe.
+ * 
+ * @author Lisa Siqueira
+ * @version 1.0
+ * @since 20/01/2023
+ */
+
+public class Calculadora {
+
+    /**
+     * <h1>Somar</h1>
+     * Este método é utilizado para somar dois inteiros
+     * @param num01
+     * @param num02
+     * @return O resultado deste método é a soma dos dois números
+     */
+}
